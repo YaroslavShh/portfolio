59929a60ff205ee62a678610bb31b347174de529
YaroslavShh <yaroslav.shamov@list.ru>
2023-08-20 16:43:32 +0300

    Add globalStyles in project

---
+++ src/components/GlobalStyle.styled.tsx
@@ -0,0 +1,21 @@
+import {createGlobalStyle} from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    outline: none;
+    border: none;
+    text-decoration: none;
+  }
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`
---
+++ src/components/MyTheme.tsx
@@ -0,0 +1,21 @@
+export const MyTheme = {
+    colors: {
+        colorText: {
+            primary: "rgba(0, 0, 0, 1)",
+            secondary: "rgba(57, 57, 57, 1)",
+            third: "rgba(255, 255, 255, 1)",
+            fourth: "rgba(108, 146, 255, 1)",
+            fifth: "rgba(139, 139, 139, 1)"
+        },
+        colorBack: {
+            primary: "rgba(166, 188, 250, 1)",
+            secondary: "rgba(33, 87, 242, 1)"
+        }
+    },
+    fontSize: {
+        primary: "60px",
+        secondary: "32px",
+        third: "20px",
+        fourth: "16px"
+    }
+}
